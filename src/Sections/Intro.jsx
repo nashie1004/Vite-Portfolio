b7d548dfc1ca21e126e9405af6c25e535e941ae9
nashie1004 <naocenar@gmail.com>
2023-04-10 21:32:04 +0800

    change button url

--- src/Sections/Intro.jsx
+++ src/Sections/Intro.jsx
@@ -10,7 +10,7 @@ export default function Intro() {
           <h1>Hi, my name is Nash</h1>
           <h1>I'm a web developer.</h1>
           <div className="btn">
-            <button onClick={() => window.location.href = 'https://drive.google.com/file/d/17jCRhTY6XYz2tjOv6xlJFgt5G8txrQDi/view?usp=share_link'}>My Resume</button>
+            <button onClick={() => window.location.href = 'https://drive.google.com/file/d/1BW8DbyOw5SyqCxviAUDFpUXjqKEHLIly/view?usp=sharing'}>My Resume</button>
           </div>
         </div>
       </div>
