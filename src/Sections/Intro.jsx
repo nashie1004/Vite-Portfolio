7fa0bd9b347eff5efafe92a2d1f6b7ab5da61292
unknown <naocenar@gmail.com>
2023-05-22 23:31:39 +0800

    change resume

--- src/Sections/Intro.jsx
+++ src/Sections/Intro.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../Styles/Intro.scss'
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default function Intro() {
   return (
@@ -10,7 +9,7 @@ export default function Intro() {
           <h1>Hi, my name is Nash.</h1>
           <h1>I'm a web developer.</h1>
           <div className="btn">
-            <button onClick={() => window.location.href = 'https://drive.google.com/file/d/1BW8DbyOw5SyqCxviAUDFpUXjqKEHLIly/view?usp=sharing'}>My Resume</button>
+            <button onClick={() => window.location.href = 'https://drive.google.com/file/d/1yzpz_WU7zb9IxNsZkVcqi45acsbaQ16q/view?usp=share_link'}>My Resume</button>
           </div>
         </div>
       </div>
